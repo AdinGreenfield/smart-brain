b2069c9a860521dcc9bd21b64b52bc5134f930f5
adin <adin@xfields.net>
2018-05-16 00:20:50 -0400

    fixing css

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,12 +4,12 @@
  const ImageLinkForm = ( { onInputChange, onSubmit  }) => {
  	return (
  			<div>
- 				<p className='f4 pa2'>
+ 				<p className='f4'>
  					{'This Magic Brain will predict the celebrity and their face in your pictures. Give it a try.'}
  				</p>
- 				<div>
+ 				<div className='center'>
  					<div className='form shadow-5 br3 pa3 center'>
- 					<input className='pa2 w-70'type='url' onChange={onInputChange} />
+ 					<input className='pa2 w-70 center'type='url' onChange={onInputChange} />
  					<button value='submit' className='grow bg-light-green link ph3 pv2 div black' onClick={onSubmit}>Detect</button>
  					</div>
  				</div>
